15156aec7bee4ae4ec9a3f3d51d2d9dfdb7b196d
JoseLuisP11 <55829252+JoseLuisP11@users.noreply.github.com>
2019-11-12 19:58:01 +0100

    Versión 1.1.0
    Se ha implementado el caso de uso Asignar un mecánico de la iteración 1.

---
+++ PhenomSoftware/Iteracion1/src/test/java/org/Iteracion1/Dominio/GestorReservas.java
@@ -0,0 +1,14 @@
+package Diseño.Dominio;
+
+public class GestorReservas {
+
+	public Reserva crearReserva() {
+
+	}
+
+	public void asignarMecanico() {
+		// TODO - implement GestorReservas.asignarMecanico
+		throw new UnsupportedOperationException();
+	}
+
+}
