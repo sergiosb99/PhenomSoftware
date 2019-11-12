270ece79d9691bcbdd07dc4fec7afecfe77cd9e7
sergiosb99 <55800055+sergiosb99@users.noreply.github.com>
2019-11-12 19:04:57 +0100

    Version 1.0.0
    Código correspondiente a la iteración 0.

---
+++ PhenomSoftware/src/main/java/C06/PhenomSoftware/App.java
@@ -0,0 +1,13 @@
+package C06.PhenomSoftware;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
