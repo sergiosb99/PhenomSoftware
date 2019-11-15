9876b56d99652faf53bf28a5a90e2460b7bc1e87
ismaelvr <52781889+ismaelvr@users.noreply.github.com>
2019-11-15 20:49:37 +0100

    Versión 1.2.0

---
+++ PhenomSoftware/Iteracion2/src/main/java/org/Iteracion2/Dominio/Box.java
@@ -0,0 +1,18 @@
+package Diseño.Dominio;
+
+public class Box {
+
+	private int id;
+	private string estado;
+
+	/**
+	 * 
+	 * @param id
+	 * @param estado
+	 */
+	public Box(int id, string estado) {
+		// TODO - implement Box.Box
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ PhenomSoftware/Iteracion2/src/main/java/org/Iteracion2/Dominio/GestorReservas.java
@@ -0,0 +1,20 @@
+package Diseño.Dominio;
+
+public class GestorReservas {
+
+	public Reserva crearReserva() {
+		// TODO - implement GestorReservas.crearReserva
+		throw new UnsupportedOperationException();
+	}
+
+	public void asignarMecanico() {
+		// TODO - implement GestorReservas.asignarMecanico
+		throw new UnsupportedOperationException();
+	}
+
+	public void realizar_reserva() {
+		// TODO - implement GestorReservas.realizar_reserva
+		throw new UnsupportedOperationException();
+	}
+
+}
