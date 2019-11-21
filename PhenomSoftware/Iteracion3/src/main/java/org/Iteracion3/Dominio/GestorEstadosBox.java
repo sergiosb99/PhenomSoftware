d196e9f8c8193e365e3d9fb874cf2697a43493fd
villamocoto <55605798+villamocoto@users.noreply.github.com>
2019-11-21 16:52:25 +0100

    Versión 1.3.0
    Implementación del caso de uso secuenciar estados del box

---
+++ PhenomSoftware/Iteracion3/src/main/java/org/Iteracion3/Dominio/GestorEstadosBox.java
@@ -0,0 +1,10 @@
+package Análisis.Dominio;
+
+public class GestorEstadosBox {
+
+	public Box secuenciarEstado() {
+		// TODO - implement GestorEstadosBox.secuenciarEstado
+		throw new UnsupportedOperationException();
+	}
+
+}
