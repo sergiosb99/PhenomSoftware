7df06eb18a16c9638e5a3dc4a88fdef6d3fd5914
sergiosb99 <55800055+sergiosb99@users.noreply.github.com>
2019-11-27 13:57:32 +0100

    Versión 1.4.0
    Implementación del caso de uso generar lista de códigos de repuesto.

---
+++ PhenomSoftware/Iteracion4/src/main/java/org/Iteracion4/Dominio/GestorListaRepuesto.java
@@ -0,0 +1,39 @@
+package Dominio;
+
+public class GestorListaRepuesto {
+
+	public ListaRepuesto crearLista() {
+		// TODO - implement GestorListaRepuesto.crearLista
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param cerrada
+	 */
+	public ListaRepuesto cerrarLista(boolean cerrada) {
+		// TODO - implement GestorListaRepuesto.cerrarLista
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param pieza
+	 * @param lista
+	 */
+	public ListaRepuesto añadirPieza(Pieza pieza, ListaRepuesto lista) {
+		// TODO - implement GestorListaRepuesto.añadirPieza
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param pieza
+	 * @param lista
+	 */
+	public ListaRepuesto eliminarPieza(Pieza pieza, ListaRepuesto lista) {
+		// TODO - implement GestorListaRepuesto.eliminarPieza
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ PhenomSoftware/Iteracion4/src/main/java/org/Iteracion4/Presentación/IU_ListaRepuesto.java
@@ -0,0 +1,10 @@
+package Presentación;
+
+public class IU_ListaRepuesto {
+
+	public IU_ListaRepuesto() {
+		// TODO - implement IU_ListaRepuesto.IU_ListaRepuesto
+		throw new UnsupportedOperationException();
+	}
+
+}
